4e83a60905d4544d8d274d8724e3ea7232fcae8a
Muhammad Saariya Faiz <saariya2899@gmail.com>
2019-10-25 16:23:24 +0500

    Update Task2b.m
    Warnings removed

--- Task2b.m
+++ Task2b.m
@@ -1,7 +1,8 @@
 X=[0 0 0 1 0 1 0 1 0 1 0 1 0 1 1 1 0 1 1 0 0 1 1 1 1 0 0 0];
 if X(1) == 0
     encode(1:100) = 5;
-else if X(1)  == 1
+else 
+    if X(1)  == 1
         encode(1:100) = -5;
     end
 end
@@ -10,14 +11,17 @@ for x = 2:length(X)
     if X(x) == 0
         if encode(i) == 5
             encode(i+1:i+100) = 5;
-        else if encode(i) == -5
+        else 
+            if encode(i) == -5
                 encode(i+1:i+100) = -5;
             end
         end
-    else if X(x) == 1
+    else 
+        if X(x) == 1
             if encode(i) == 5
                 encode(i+1:i+100) = -5;
-            else if encode(i) == -5
+            else 
+                if encode(i) == -5
                     encode(i+1:i+100) = 5;
                 end
             end
